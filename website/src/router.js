3a937e3228374cbe25054d380e2186c229c562f6
newlls <523786175@qq.com>
2018-11-22 18:02:34 +0800

    website

--- website/src/router.js
+++ website/src/router.js
@@ -4,8 +4,12 @@ import Home from './views/Home.vue'
 import Vitae from './views/Vitae.vue'
 import Blog from './views/Blog.vue'
 import SampleReels from './views/SampleReels.vue'
-
-Vue.use(Router)
+import LSkill from './views/LSkill.vue'
+import About from './views/About.vue'
+import ZSkill from './views/ZSkill.vue'
+import ZSampleReels from './views/ZSampleReels.vue'
+import ZAbout from './views/ZAbout.vue'
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -31,6 +35,31 @@ export default new Router({
           path: '/samplereels',
           name: 'samplereels',
           component: SampleReels
+      },
+      {
+          path: '/lskill',
+          name: 'lskill',
+          component: LSkill
+      },
+      {
+          path:'/about',
+          name:'about',
+          component:About
+      },
+      {
+          path:'/zskill',
+          name:'zskill',
+          component:ZSkill
+      },
+      {
+          path:'/zsamplereels',
+          name:'zsamplereels',
+          component:ZSampleReels
+      },
+      {
+          path:'/zabout',
+          name:'zabout',
+          component:ZAbout
       }
   ]
 })
