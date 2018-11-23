e6a114937ad66c44037949d37e151e3e2e59a7e7
TLROJE787 <38369651+TLROJE787@users.noreply.github.com>
2018-11-23 11:09:36 +0800

    Add files via upload

--- website/src/App.vue
+++ website/src/App.vue
@@ -10,12 +10,9 @@
 
 <style>
 html,body,#app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50; */
   height: 100%;
+      z-index: -1;
+    position: relative;
 }
 *{
   margin: 0;
